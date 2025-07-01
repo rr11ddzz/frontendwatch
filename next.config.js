be761c27405bd85aff975e1dd0c2dfcd6a675ec2
moiz2194 <moiz23032194@gmail.com>
2025-07-01 23:40:46 +0500

    sddsds

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: false,
   env: {
     NEXT_APP_HOST:"https://backendwatch-production.up.railway.app",
-    NEXT_APP_STRIPE_PUBLISHABLE_KEY:'pk_test_51PBaCGKv2xIKQkngGij2fLZ6TDiXPNS0dKJJ81UZ3ePUSSc0I5sWq492Vax6PlJFUdXk08YmqOymQ63cY7s9I3qz00VU6Xz0Jv'
+    NEXT_APP_STRIPE_PUBLISHABLE_KEY:'pk_test_51O5CTTJ4JWXOoJd9vnv0QjnUInNTF5xQHUiOxA1sZR74IMQespfwfBTStVq3pkKkSB7ZlJAywbWPxwheeSDE0f1b000CSKi9Fd'
     },
   images:{
     domains:["res.cloudinary.com"]
